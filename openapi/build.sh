c984f47341e07ee292afef68a2702b3ca56fe54c
Matthew Slipper <me@matthewslipper.com>
2019-10-30 14:39:41 -0700

    Website release Wed Oct 30 14:39:41 PDT 2019

--- openapi/build.sh
+++ openapi/build.sh
@@ -1,18 +1,13 @@
 #!/usr/bin/env bash
 set -e
 
-git checkout master
+rm -rf ./codegen-out
+rm -rf ./build
 mkdir ./codegen-out
 mkdir ./build
 ./node_modules/.bin/sc generate -l openapi -i ./openapi.yml -o ./codegen-out
-git checkout release
-git merge master --no-edit
 cp ./codegen-out/openapi.json ./build
-cp ./dex-demo-embedded.html ./build
+cp ./dex-demo-embedded.html ./build/index.html
 git add -f ./build
 now=$(date)
 git commit -am "Website release $now"
-git push origin release
-rm -rf ./build
-rm -rf ./codegen-out
-git checkout master
